88821f40bd84bf24ee23c6f51e75af8a974fadee
Michael Jost <michael.jost@misurio.ch>
2021-03-04 23:34:31 +0100

    gallery dynamic

---
+++ ncr/2006-gv.js
@@ -0,0 +1,15 @@
+var gallery2006Gv = {
+  load: function(object, options) {
+    var links= {
+      dynamicEl: [{
+        'src': 'https://lh3.googleusercontent.com/kpFIsOBOBMRE_HsQMDWg3ctUVOvuBQV4CnOpD3AzzouL0R5QGzvwcVIsltTruFC9vYaF5ZO9v2jncQgGsMWWsMUZvffJ6nB0dE8ziLJ9h3BZe2N4GACYI5Voycm6yJCU7dS7Dx35mw=w2000-h2000',
+        'thumb': 'https://lh3.googleusercontent.com/kpFIsOBOBMRE_HsQMDWg3ctUVOvuBQV4CnOpD3AzzouL0R5QGzvwcVIsltTruFC9vYaF5ZO9v2jncQgGsMWWsMUZvffJ6nB0dE8ziLJ9h3BZe2N4GACYI5Voycm6yJCU7dS7Dx35mw=w100-h100'
+      }, {
+        'src': 'https://lh3.googleusercontent.com/yXU1-mE7y7k4AoBaRT1cD7aOWHMy4MhhT9R08k8_nbBhLOySESldLko8Wnu-3bdJ0FdbuPFZDmhgDe8cagQLWDRQauX1QdjsMwgnoEFYkadImTHNB9EpjL3GnLn-0ssUWhZL-B_tZQ=w2000-h2000',
+        'thumb': 'https://lh3.googleusercontent.com/yXU1-mE7y7k4AoBaRT1cD7aOWHMy4MhhT9R08k8_nbBhLOySESldLko8Wnu-3bdJ0FdbuPFZDmhgDe8cagQLWDRQauX1QdjsMwgnoEFYkadImTHNB9EpjL3GnLn-0ssUWhZL-B_tZQ=w100-h100'
+      }
+      ]
+    }
+    object.lightGallery(Object.assign(options, links));
+  }
+}
